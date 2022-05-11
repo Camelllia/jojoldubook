09b81d1a3be36e4e06222cc2259583776663fc50
KimSeungJin <dahyun101107@naver.com>
2022-05-11 16:37:45 +0900

    Update: Google Social Login

--- book/src/main/java/com/jojoldu/book/Controller/IndexController.java
+++ book/src/main/java/com/jojoldu/book/Controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.Controller;
 
 import com.jojoldu.book.Dto.PostsResponseDto;
+import com.jojoldu.book.config.auth.LoginUser;
+import com.jojoldu.book.config.auth.dto.SessionUser;
 import com.jojoldu.book.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName" , user.getName());
+        }
+
         return "index";
     }
 
