ba42699e4d507fc900bf3d6de0c79568d6defef6
KimSeungJin <dahyun101107@naver.com>
2022-05-11 17:58:45 +0900

    Update: Style

--- book/src/main/java/com/jojoldu/book/Controller/IndexController.java
+++ book/src/main/java/com/jojoldu/book/Controller/IndexController.java
@@ -44,7 +44,7 @@ public class IndexController {
         if(user != null) {
             model.addAttribute("userName" , user.getName());
         }
-        
+
         model.addAttribute("post", dto);
 
         return "posts-update";
