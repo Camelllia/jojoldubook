05aa36413d5e82cef35c7626d2eaad4a32fbb19f
KimSeungJin <dahyun101107@naver.com>
2022-05-11 14:59:20 +0900

    Update: Delete, Update, OAuth Setting

--- book/src/main/java/com/jojoldu/book/service/posts/PostsService.java
+++ book/src/main/java/com/jojoldu/book/service/posts/PostsService.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.service.posts;
 
+import com.jojoldu.book.Dto.PostsListResponseDto;
 import com.jojoldu.book.Dto.PostsResponseDto;
 import com.jojoldu.book.Dto.PostsSaveRequestDto;
 import com.jojoldu.book.Dto.PostsUpdateRequestDto;
@@ -8,6 +9,9 @@ import com.jojoldu.book.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +41,21 @@ public class PostsService {
                         IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다 id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
